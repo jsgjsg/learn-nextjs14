276622b638bb7489cdb8b404dc39e90200809d8e
jsgjsg <jsgcys0815@gmail.com>
2025-05-05 15:57:23 +0900

    #3 DATA FETCHING 완료

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,17 @@
+import Link from "next/link";
 import { resolve } from "path";
 
 export const metadata = {
   title: 'Home'
 }
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
 
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  const response = await fetch(URL);
+  const response = await fetch(API_URL);
   const json = await response.json();
   return json;
 }
@@ -19,6 +20,10 @@ export default async function HomePage() {
   const movies = await getMovies();
 
   return <div>
-    {JSON.stringify(movies)}
+    {movies.map((movie) => (
+      <li key={movie.id}>
+        <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+      </li>
+    ))}
   </div>
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,4 +1,19 @@
-export default function MovieDetail({params: {id}}: {params: { id: string }} ) {
+import { Suspense } from "react";
+import { API_URL } from "../../../(home)/page";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
-  return <h1>Movie {id}</h1>;
+
+export default async function MovieDetail({params: {id}}: {params: { id: string }} ) {
+
+  return (
+    <div>
+      <Suspense fallback={<h2>Loading movie info</h2>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h2>Loading movie videos</h2>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
