2c47664b0638a1baafe538d07eed6a01e60be0af
jsgjsg <jsgcys0815@gmail.com>
2025-05-05 16:23:01 +0900

    #4.1 DEPLOYMENT - CSS Modules 까지 완료

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -3,10 +3,10 @@ import { API_URL } from "../app/(home)/page";
 async function getVideos(id: string) {
     await new Promise((resolve) => setTimeout(resolve, 1000));
     
-    throw new Error("Something broken");
+    // throw new Error("Something broken");
 
-    // const response = await fetch(`${API_URL}/${id}/videos`);
-    // return response.json();
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return response.json();
 }
 
 export default async function MovieVideos({id} : { id: string }) {
