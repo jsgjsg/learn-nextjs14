ca729bf1804212972007d37887fb311ea81415f3
jsgjsg <jsgcys0815@gmail.com>
2025-06-01 21:56:40 +0900

    #4.5 DEPLOYMENT - Deployment 배포 전 커밋

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,10 +1,26 @@
 import { Suspense } from "react";
 import { API_URL } from "../../../(home)/page";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
+interface IParams {
+  params: { id: string };
+}
 
-export default async function MovieDetail({params: {id}}: {params: { id: string }} ) {
+export async function generateMetadata({
+  params: { id }
+}: IParams) {
+  
+  const movie = await getMovie(id);
+  
+  return {
+    title: movie.title
+  }
+}
+
+export default async function MovieDetailPage({
+  params: { id }
+}: IParams) {
 
   return (
     <div>
