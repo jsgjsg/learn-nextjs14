9091995aa8d01792b6eb83583d2b6dd9bca5d9d3
jsgjsg <jsgcys0815@gmail.com>
2025-06-02 22:30:38 +0900

    [FIX] params를 Promise로 받도록 수정

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,14 +2,14 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-interface IParams {
-  params: { id: string };
-}
+type IParams = Promise<{
+  id: string;
+}>;
 
-export async function generateMetadata({
-  params: { id }
-}: IParams) {
-  
+export async function generateMetadata(props: { params: IParams }) {
+
+  const params = await props.params;
+  const id = params.id;
   const movie = await getMovie(id);
   
   return {
@@ -17,9 +17,9 @@ export async function generateMetadata({
   }
 }
 
-export default async function MovieDetailPage({
-  params: { id }
-}: IParams) {
+export default async function MovieDetailPage(props: { params: IParams }) {
+  const params = await props.params;
+  const id = params.id;
 
   return (
     <div>
